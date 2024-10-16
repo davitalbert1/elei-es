ce1400918b9e858491b7bdb483f28adb166a116b
davitalbert1 <128089622+davitalbert1@users.noreply.github.com>
2024-10-16 20:12:46 -0300

    Delete cadastro directory

--- cadastro/index.js
+++
@@ -1,6 +0,0 @@
-function showCadastro() {
-    document.getElementById('cadastro').style.display = 'block';
-    setTimeout(function() {
-        window.location.href = window.location.origin + '/pesquisa/pesquisa.html';
-    }, 1000);
-  }
