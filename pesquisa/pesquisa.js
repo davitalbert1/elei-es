6dda324b6210c9bcc8c85ce92e1cecb5001fc0a8
davitalbert1 <128089622+davitalbert1@users.noreply.github.com>
2024-10-16 20:12:38 -0300

    Delete pesquisa directory

--- pesquisa/pesquisa.js
+++
@@ -1,5 +0,0 @@
-let votes = [0, 0, 0, 0]; // array para armazenar os votos
-function vote(candidate) {
-    votes[candidate - 1]++; // incrementa o voto do candidato escolhido
-    alert(`Você votou no candidato ${candidate}!`);
-}
